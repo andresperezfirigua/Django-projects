43dbababfb65a16b1a410bcfe0a02db7d7f657df
Andres <andresperezfirigua@gmail.com>
2024-07-09 19:49:11 -0500

    Created 1:Many, Many:Many and 1:1 database relationships in this Django project

--- Foundations/first_project/first_app/models.py
+++ Foundations/first_project/first_app/models.py
@@ -8,3 +8,33 @@ class Car(models.Model):
 
     def __str__(self) -> str:
         return f'{self.title} - {self.year} - {self.color}'
+    
+class Publisher(models.Model):
+    name = models.TextField(max_length=200)
+    address = models.TextField(max_length=300)
+
+    def __str__(self) -> str:
+        return self.name
+    
+
+class Author(models.Model):
+    name = models.TextField(max_length=200)
+    birth_date = models.DateField()
+
+    def __str__(self) -> str:
+        return self.name
+
+class Profile(models.Model):
+    author = models.OneToOneField(Author, on_delete=models.CASCADE)
+    website = models.URLField(max_length=200)
+    biography = models.TextField(max_length=500)
+
+
+class Book(models.Model):
+    title = models.TextField(max_length=200)
+    publication_date = models.DateField()
+    publisher = models.ForeignKey(Publisher, on_delete=models.CASCADE)
+    authors = models.ManyToManyField(Author, related_name='authors')
+
+    def __str__(self) -> str:
+        return self.title
