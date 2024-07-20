dacb4c61082f40b8767ce054e22d07ccd7799369
Andres <andresperezfirigua@gmail.com>
2024-07-19 19:37:24 -0500

    Changed Car view to retrieve cars list from DB

--- Foundations/first_project/first_app/views.py
+++ Foundations/first_project/first_app/views.py
@@ -1,12 +1,11 @@
 from django.shortcuts import render
+from first_app.models import Car
 
 # Create your views here.
 def my_view(request):
-    car_list = [
-        {'title' : 'BMW'},
-        {'title' : 'Mechita'}
-    ]
+    cars_list = Car.objects.all()
+
     context = {
-        'car_list' : car_list
+        'car_list' : cars_list
     }
     return render(request, "first_app/car_list.html", context)
