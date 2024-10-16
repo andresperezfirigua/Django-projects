d7e7f7efa211d2c7e3aef9bc2ac024ce32e5989c
Andres <andresperezfirigua@gmail.com>
2024-10-16 18:26:37 -0500

    Add ListProductsView

--- Coffee_Shop/coffee_shop/products/models.py
+++ Coffee_Shop/coffee_shop/products/models.py
@@ -5,8 +5,10 @@ class Product(models.Model):
     name = models.TextField(max_length=200, verbose_name="nombre")
     description = models.TextField(max_length=300, verbose_name="description")
     price = models.DecimalField(max_digits=10, decimal_places=2, verbose_name="precio")
-    available = models.BooleanField(default=True, verbose_name='disponible')
-    picture = models.ImageField(upload_to='logos', null=True, blank=True, verbose_name='foto')
-    
+    available = models.BooleanField(default=True, verbose_name="disponible")
+    picture = models.ImageField(
+        upload_to="logos", null=True, blank=True, verbose_name="foto"
+    )
+
     def __str__(self) -> str:
         return self.name
--- Coffee_Shop/coffee_shop/products/views.py
+++ Coffee_Shop/coffee_shop/products/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from django.views.generic import TemplateView
+from .models import Product
 
-# Create your views here.
+
+class ListProductsView(TemplateView):
+    template_name = "products/products_list.html"
+
+    def get_context_data(self):
+        return {"products_list": Product.objects.all().order_by("name")}
