da7bef59eed17ba908f9d8fb6f6e1da5331c7545
Andres <andresperezfirigua@gmail.com>
2024-11-13 19:45:14 -0500

    Updated Product model

--- Coffee_Shop/coffee_shop/products/models.py
+++ Coffee_Shop/coffee_shop/products/models.py
@@ -3,10 +3,10 @@ from django.db import models
 
 class Product(models.Model):
     name = models.TextField(max_length=200, verbose_name="nombre")
-    description = models.TextField(max_length=300, verbose_name="description")
-    price = models.DecimalField(max_digits=10, decimal_places=2, verbose_name="precio")
-    available = models.BooleanField(default=True, verbose_name="disponible")
-    picture = models.ImageField(upload_to="images/", null=True, blank=True, verbose_name="foto")
+    description = models.TextField(max_length=300)
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    available = models.BooleanField(default=True)
+    picture = models.ImageField(upload_to="images/", null=True, blank=True)
     created_date = models.DateTimeField(auto_now_add=True)
 
     def __str__(self) -> str:
